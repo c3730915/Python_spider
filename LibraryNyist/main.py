50ee9d1e9e9d5e8fe339bf745b6a22376517553b
c3730915 <730915@outlook.com>
2021-03-10 21:48:49 +0800

    完成最基本的登陆验证，获取token，筛选座位id等功能

--- LibraryNyist/main.py
+++ LibraryNyist/main.py
@@ -1,41 +1,63 @@
-from selenium import webdriver
-import urllib.request
-import requests
-import time
-import json
-import chaojiying
 import re
 
-
-def getCookie():
-    driver = webdriver.Chrome("C:\Program Files\Google\Chrome\Application\chromedriver.exe")
-    driver.get("http://122.207.209.2/home/web/seat/area/1")
-    el = driver.find_element_by_class_name("login-btn").click()
-    driver.find_element_by_name("username").send_keys("1815925161")
-    driver.find_element_by_name("password").send_keys("000")
-    img = driver.find_element_by_id("checkpic").screenshot_as_png
-    cookies = driver.get_cookies()
-    f = open("cookie.json", "w")
-    f.write(cookies.__str__())
-    f.close()
-    driver.close()
-    exit()
-    # f = open("test.png", "wb")
-    # f.write(img)
-    # f.close()
-    # chaojiying = chaojiying.
-    # im = open('test.png', 'rb').read()  # 本地图片文件路径 来替换 a.jpg 有时WIN系统须要//
-    res = chaojiying.PostPic(img, 4004)
-    print(res["pic_str"])
-    driver.find_element_by_name('verify').send_keys(res["pic_str"])
-    driver.find_element_by_class_name("ui-dialog-autofocus").click()
+import chaojiying
+import requests
 
 
-def getFreeRoom():
-    # url = "http://122.207.209.2/api.php/space_time_buckets?day=2021-3-7&area=7"
-    url = "http://122.207.209.2/api.php/spaces_old?area=7&segment=1303812&day=2021-03-07&startTime=08%3A00&endTime=12%3A00"
+def getCode():
+    # 直接返回一个验证码，方便登录！
+    url = "http://122.207.209.2/api.php/check"
+    payload = {}
     headers = {
-        'Referer': 'http://112.207.209.2/web/seat3?area=6&segment=1302598&day=2021-3-7&startTime=07:50&endTime=12:00',
+        'Referer': 'http://122.207.209.2/home/web/seat/area/1',
+        'Cookie': 'PHPSESSID=eecgaq2licine3bu94b6oi1ts4'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    code = verifyCode(response.content)
+    return code
+
+def login(username="1815925161",password="c6f057b86584942e415435ffb1fa93d4"):
+    # 登录函数
+    print("Getting verify code.....")
+    verifyCode = getCode()
+
+    url = "http://122.207.209.2/api.php/login?username="+username.__str__()+"&password="+password.__str__()+"&verify="+verifyCode
+    # print(url)
+    # return
+
+    payload = {}
+    headers = {
+        'Referer': 'http://122.207.209.2/home/web/seat/area/1',
+        'Cookie': 'PHPSESSID=eecgaq2licine3bu94b6oi1ts4'
+    }
+    print("Trying to log in....")
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return re.findall(r'access_token":"(.*?)"', response.text)[0]
+
+def verifyCode(img):
+    client = chaojiying.Chaojiying_Client('c3730915', '9DpxzsJbNBJLB.F', '913619')
+    data = client.PostPic(img,"4004")
+    print(data)
+    return (data['pic_str'])
+
+def getFreeRoom(area="8",year="2021",month="03",day="11",flag =0):
+    if(flag==0):
+        #上午进行预定
+        startTime = "08"
+        endTime = "12"
+    else:
+        startTime = "14"
+        endTime = "22"
+
+    # 2021 - 3 - 11 & startTime = 14:30 & endTime = 21:30
+    # 参数说明：area 预定区域，剩下三个为年月日
+    # flag代表是上午还是晚上，上午为0，下午为1
+    #开始时间还可以为14:30 结束时间为21:30
+    url = "http://122.207.209.2/api.php/spaces_old?area="+area+"&segment=1303812&day="+year+"-"+month+"-"+day+"&startTime="+startTime+"%3A00&endTime="+endTime+"%3A00"
+    headers = {
+        'Referer': 'http://112.207.209.2/web/seat3?area=6&segment=1302598&day=2021-3-11&startTime=07:50&endTime=12:00',
         'Cookie': 'PHPSESSID=j007enuovkd4c0r0l7o579g5a7'}
 
     response = requests.request("GET", url, headers=headers)
@@ -43,55 +65,25 @@ def getFreeRoom():
 
     return response.json()['data']['list']
 
+def getSeat(seatList):
+    for seat in seatList:
+        if (seat['status'] == 1):
+            return seat['id']
+accessToken = login()
+print(accessToken+"登陆成功！")
 
-def getParm(str):
-    re.search()
+freeSeats = getFreeRoom()
+print("已经发现空位，空位id是：")
+print(getSeat(freeSeats))
 
-
-def readCookie():
-    with open('cookie.json') as file:
-        fileContent = file.read()
-        items = fileContent.split("}")
-        dict = {}
-        for item in items:
-            if (item.__len__() > 10):
-                dict[re.findall("'name': '(.*?)',", item.__str__())[0]] = \
-                re.findall("'value': '(.*?)'", item.__str__())[0]
-
-        return dict
+# print(type(getFreeRoom()))
 
 
-readCookie()
-exit()
-# Chrome操作阶段
-driver = webdriver.Chrome("C:\Program Files\Google\Chrome\Application\chromedriver.exe")
-driver.get("http://122.207.209.2/home/web/seat/area/1")
-el = driver.find_element_by_class_name("login-btn").click()
-driver.find_element_by_name("username").send_keys("1815925161")
-driver.find_element_by_name("password").send_keys("000")
-
-# 识别验证码
-chaojiying = chaojiying.Chaojiying_Client('c3730915', '9DpxzsJbNBJLB.F', '913619')  # 用户中心>>软件ID 生成一个替换 96001
-img = driver.find_element_by_id("checkpic").screenshot_as_png
-res = chaojiying.PostPic(img, 4004)
-driver.find_element_by_name('verify').send_keys(res["pic_str"])
-driver.find_element_by_class_name("ui-dialog-autofocus").click()
-time.sleep(4)
-# 获取cookie
-cookies = driver.get_cookies()
-if (cookies.__len__() == 6):
-    print("成功登录！")
-    f = open("cookie.json", "w")
-    f.write(cookies.__str__())
-    f.close()
+result = login()
+if(result.__len__()<=10):
+    print("Log in error! Please try again!")
 else:
-    print("登陆失败！请重新登陆")
+    print("Log in successfully! Token is" +result )
 
-driver.close()
-exit()
 
-print(getFreeRoom())
-f = open("free.json", "w", encoding='UTF-8')
-f.write(json.dumps(getFreeRoom()))
-f.close()
-# run()
+
--- LibraryNyist/test.py
+++ LibraryNyist/test.py
@@ -1,6 +1,19 @@
-import json
-test_json = json.dumps({ 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 })
-pairs = json.loads("{ 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 }")
-pairs.i
-# print(test_json['a'])
-str = ""
+import requests
+
+url = "http://122.207.209.2/api.php/check"
+
+payload={}
+headers = {
+  'Accept':'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+  'Accept-Encoding':'gzip, deflate',
+  'Accept-Language':'en,en-US;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+  'Connection':'keep-alive',
+  'Cookie': 'PHPSESSID=c3ep7l74n7sapu1bfenvpdujl3; uservisit=1; redirect_url=%2Fhome%2Fweb%2Fseat%2Farea%2F1; PHPSESSID=gvu5sm88eoiu6bovvic8pp5ej0',
+  'Host': '122.207.209.2',
+  'Referer':'http://122.207.209.2/home/web/seat/area/1',
+  'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
