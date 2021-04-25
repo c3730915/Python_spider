3acc9c100c87fbc6f7b3cdac38af64038eebc61e
c730915 <1023700418@qq.com>
2021-04-26 00:23:19 +0800

    完成基本的爬取，明天进一步优化！

--- GetEnglishBookLists/main.py
+++ GetEnglishBookLists/main.py
@@ -1,9 +1,67 @@
+import urllib
+
 from bs4 import BeautifulSoup
 import requests
+import re
 
-html = "http://cmpbook.com/searchbook.php?pagestart=0&action=search&title=&series=%BE%AD%B5%E4%D4%AD%B0%E6&isbn=&publictime=&storetime=&searchword=&s1=&s2=&Submit=%CB%D1%CB%F7&orderby=publictime"
-content = requests.get(html)
-# print(content.content)
-bs = BeautifulSoup(content, "html.parser")
+def askURL(url):
+    head = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36"
+
+    }
+    #请求头，伪装成浏览器
+    request = urllib.request.Request(url, headers=head)
+    html = ""
+    try:
+        response = urllib.request.urlopen(request)
+        #运用urllib库来进行请求
+        html = response.read().decode("gbk")
+        #返回html页面数据，utf-8方式乱码
+        # print(html)
+    except urllib.error.URLError as e:
+        if hasattr(e, "code"):
+            print(e, "code")
+
+        if hasattr(e, "reason"):
+            print(e.reason)
+    return html
+
+def saveFile(html):
+    # str =
+    open('text.html', 'w').write(html)
+html = open('text.html','r').read()
+# re.findall(r"")
+bs = BeautifulSoup(html, "html.parser")
+sub = bs.findAll('table', {"class": "table01","width": "400", "align":"left"})
+
+re.findall('src="data/stackroom/(.*?)"',str(html))
+re_id=re.compile('id=(.*?)"')
+re_title=re.compile('target="_blank"><span class="font09">(.*?)</')
+re_author=re.compile('<td align="left"><span class="font01">(.*?)<')
+re_ISBN=re.compile('<span class="font01">ISBN(.*?)<')
+re_series=re.compile('<span class="font01">(.*?)</span>')
+
+for item in sub:
+    # print(item)
+    tmp_str = str (item)
+    # print(re.findall(re_title,tmp_str))
+    # print(re.findall(re_id,tmp_str))
+    # print(re.findall(re_author,tmp_str))
+    isbn_str = re.findall(re_ISBN,tmp_str).__str__()
+    # isbn_str = isbn_str[isbn_str.find('：')+1:-2]
+    # print(isbn_str)
+    # print(re.findall(re_series,tmp_str)[2])
+    print("=================")
+# # print(sub[4])
+# for th in sub:
+#     print(th.findAll('table', {"width": "95%", "class": "table01"}))
+# result = sub.findAll('table', {"width": "95%", "class": "table01"})
+# print(result)
+
+
+# for th in sub:
+#     # print(th.findAll('table', {"width": "95%", "class": "table01"}))
+
+
 
 
